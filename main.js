ce7020dc6508e32ae6334f7da5f8d243e11422da
Gabriel <gabriel.victorc13@gmail.com>
2020-07-18 18:52:21 -0300

    Versão 1.1.1

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ const path = require('path')
 
 function createWindow () {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({width: 600, height: 600, webPreferences: {preload: path.join(__dirname, 'preload.js')}
+  const mainWindow = new BrowserWindow({
+  	width: 600, 
+  	height: 600, 
+  	icon:__dirname + "/aelle.png",
+  	webPreferences: {
+  		preload: path.join(__dirname, 'preload.js')
+  	}
   })
 	mainWindow.setMenuBarVisibility(false)
   // and load the index.html of the app.
